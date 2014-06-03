3ed5fff6ae0cf0d5fb972a884cce034b2a42c74d
courtney mcilhenny <github@courtneymcilhenny>
2014-06-03 09:18:43 -0700

    added methods for update and delete

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -17,4 +17,23 @@ class TodosController < ApplicationController
   	end
   end
 
+  def update
+    todo_params = params.require(:todo).permit(:completed)
+    @todo = Todo.find(params[:id])
+    @todo.update_attributes todo_params
+
+    respond_to do |f|
+      f.json { render :json => @todo, only: [:id, :title, :completed]}
+    end
+  end
+
+  def delete
+    @todo = Todo.find(params[:id])
+    @todo.delete
+
+    respond_to do |f|
+      f.json { render :json => @todo, only: [:id, :title, :completed]}
+    end
+  end
+
 end
